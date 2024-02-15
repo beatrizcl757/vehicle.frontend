53236e7f130507c1eb9c34c1a77d12828d4902ac
beatrizcl757 <beatrizcl757@gmail.com>
2024-02-15 11:01:02 -0300

    crud using spring boot and angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { VehicleInputComponent } from './vehicle-input/vehicle-input.component';
+import { VehicleDisplayComponent } from './vehicle-display/vehicle-display.component';
+import { VehicleEditComponent } from './vehicle-edit/vehicle-edit.component';
+import { VehicleWrapperComponent } from './vehicle-wrapper/vehicle-wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    VehicleInputComponent,
+    VehicleDisplayComponent,
+    VehicleEditComponent,
+    VehicleWrapperComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
